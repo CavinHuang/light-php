18f461235e8e4ef1e5a8e68641f29961b983b9ab
CavinHuang <943866180@qq.com>
2018-06-26 14:13:16 +0800

    feat(add project): init project
    初始化整个框架项目
    
    BREAKING CHANGE: 初始化整个项目

---
+++ framework/run.php
@@ -0,0 +1,10 @@
+<?php
+/*************************************************
+ *                  light PHP                    *
+ *                                               *
+ *    A Lightweight Full-Stack PHP Framework     *
+ *                                               *
+ *                  cavinHuang                   *
+ *        <https://github.com/TIGERB>            *
+ *                                               *
+ *************************************************/
