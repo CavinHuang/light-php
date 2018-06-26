007f1a601589181f923959d00e25310dd589f5d3
CavinHuang <943866180@qq.com>
2018-06-26 14:21:24 +0800

    feat(add project  folder to worke flow): init project for working

---
+++ app/demo/controllers/Index.php
@@ -0,0 +1,20 @@
+<?php
+/*************************************************
+ *                  light PHP                    *
+ *                                               *
+ *    A Lightweight Full-Stack PHP Framework     *
+ *                                               *
+ *                  cavinHuang                   *
+ *        <https://github.com/TIGERB>            *
+ *                                               *
+ *************************************************/
+
+namespace app\demo\controllers;
+
+class Index {
+
+  public function index () {
+
+    return 1;
+  }
+}
